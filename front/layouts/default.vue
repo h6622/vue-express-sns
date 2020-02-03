dc99dc495427a36fe7a5c76075a47e7c473e812e
h662 <godh662@gmail.com>
2020-02-03 17:15:26 +0900

    add vuex

--- front/layouts/default.vue
+++ front/layouts/default.vue
@@ -1,14 +1,9 @@
 <template>
   <v-app>
     <div>
-      <v-toolbar
-        dark
-        color="green"
-      >
+      <v-toolbar dark color="green">
         <v-toolbar-title>
-          <nuxt-link to="/">
-            NodeBird
-          </nuxt-link>
+          <nuxt-link to="/">NodeBird</nuxt-link>
         </v-toolbar-title>
         <v-spacer />
         <v-toolbar-items>
@@ -18,34 +13,22 @@
             prepend-icon="mdi-magnify"
             :style="{ display: 'flex', alignItems: 'center' }"
           />
-          <v-btn
-            text
-            nuxt
-            to="/profile"
-          >
+          <v-btn text nuxt to="/profile">
             <div>프로필</div>
           </v-btn>
-          <v-btn
-            text
-            nuxt
-            to="/signup"
-          >
+          <v-btn text nuxt to="/signup">
             <div>회원가입</div>
           </v-btn>
         </v-toolbar-items>
       </v-toolbar>
     </div>
+    <div>{{name}}</div>
+    <v-btn @click="onChangeName">바이바이</v-btn>
     <v-row no-gutters>
-      <v-col
-        cols="12"
-        md="4"
-      >
+      <v-col cols="12" md="4">
         <login-form />
       </v-col>
-      <v-col
-        cols="12"
-        md="8"
-      >
+      <v-col cols="12" md="8">
         <nuxt />
       </v-col>
     </v-row>
@@ -58,6 +41,16 @@ import LoginForm from "~/components/LoginForm";
 export default {
   components: {
     LoginForm
+  },
+  computed: {
+    name() {
+      return this.$store.state.posts.hello;
+    }
+  },
+  methods: {
+    onChangeName() {
+      this.$store.commit("posts/bye");
+    }
   }
 };
 </script>
